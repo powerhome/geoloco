4eb68506e2975e1e323261b15a9e5e73f5a4c0ac
Carlos Palhares <chjunior@gmail.com>
2019-11-07 14:47:09 -0300

    Initialize geoloco repository

---
+++ lib/geoloco.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'geoloco/version'
+
+module Geoloco
+  class Error < StandardError; end
+end
---
+++ lib/geoloco/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Geoloco
+  VERSION = '0.1.0'
+end
